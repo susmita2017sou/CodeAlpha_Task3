172eaf229a253911fe4bdcc848ec95a80ed37b63
@Susmita <156946010+susmita2017sou@users.noreply.github.com>
2025-05-17 21:11:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert("Thank you! Your message has been sent.");
+    this.reset();
+  });
+  
